2bd092ce72422cf4744272ce3b79ab6d4bcd02e6
Badeggs11 <84923646+Badeggs11@users.noreply.github.com>
2025-03-15 22:02:52 +0000

    adiciona pages/

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <h1> Sobre Nos </h1>
+            <p>Esta eh a pagina sobre nosso projeto</p>
+        </div>
+    );
+}
+
+
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1> Bem vindo trouxas, seremos trouxas tb</h1>
+        </div>
+    );
+}
